a457ab6a54e466995baf43501ab95c9cc84e84c1
KoheiAsano <kk.asano.luxy@gmail.com>
2019-12-07 15:56:59 +0900

    SegmentTree and UnionFind

---
+++ src/lib/DataStructure/SegmentTree.rs
@@ -0,0 +1,83 @@
+struct SegTree {
+    // size: 葉の数, velue: ノードの値, neutral: 単位元, calc: 計算関数(isize, isize) -> isize
+    num: usize,
+    value: Vec<isize>,
+    neutral: isize,
+    calc: Box<Fn((isize, isize)) -> isize>,
+}
+
+impl SegTree {
+    fn new(size: usize, neutral: isize, calc: Box<Fn((isize, isize)) -> isize>) -> Self {
+        // セグ木にできる分のNodeの数を計算
+        // size以上の最小の(2の冪の2倍-1)にしたい
+        let i = size.checked_next_power_of_two().unwrap();
+        let n = 2 * i - 1;
+        let value: Vec<isize> = (0..n).map(|_| neutral).collect();
+        SegTree {
+            num: i,
+            value: value,
+            neutral: neutral,
+            calc: calc,
+        }
+    }
+    // 点更新, i番目の値をxで更新
+    fn update(&mut self, i: usize, x: isize) {
+        let mut i = i + self.num - 1; // 対応する葉のNodeへ
+        self.value[i] = x;
+        while i > 0 {
+            i = (i - 1) / 2;
+            // 親の値を更新する
+            self.value[i] = (self.calc)((self.value[i * 2 + 1], self.value[i * 2 + 2]));
+        }
+    }
+    // [a, b): クエリの区間, k: valueのNode, [l,r): k-Nodeの担当区間
+    fn query(&self, a: usize, b: usize, k: usize, l: usize, r: usize) -> isize {
+        if r <= a || b <= l {
+            self.neutral // 区間がかぶらないので単位元
+        } else if a <= l && r <= b {
+            self.value[k] //もろの区間なので値を返す
+        } else {
+            //半端な区間なので左右にqueryしてもう一回評価をする
+            let l_val = self.query(a, b, 2 * k + 1, l, (l + r) / 2);
+            let r_val = self.query(a, b, 2 * k + 2, (l + r) / 2, r);
+            (self.calc)((l_val, r_val))
+        }
+    }
+}
+
+fn main() {
+}
+
+#[cfg(test)]
+mod tests {
+    use super::SegTree;
+
+    #[test]
+    fn check_min() {
+        let mut st = SegTree::new(
+            3,
+            std::isize::MAX,
+            Box::new(|(a, b): (isize, isize)| -> isize { std::cmp::min(a, b) }),
+        );
+        st.update(0, 1);
+        st.update(1, 2);
+        st.update(2, 3);
+        // 1-indexedで来たらa-1, b
+        // 0-indexedで来たらa, b+1
+        assert_eq!(st.query(0, 3, 0, 0, st.num), 1);
+        assert_eq!(st.query(1, 3, 0, 0, st.num), 2);
+    }
+
+    #[test]
+    fn check_sum() {
+        let mut st = SegTree::new(3, 0, Box::new(|(a, b): (isize, isize)| -> isize { a + b }));
+        st.update(0, 1);
+        st.update(1, 2);
+        st.update(2, 3);
+        // [0,2)
+        // [1,2)
+        assert_eq!(st.query(0, 2, 0, 0, st.num), 3);
+        assert_eq!(st.query(1, 2, 0, 0, st.num), 2);
+    }
+
+}
---
+++ src/lib/DataStructure/UnionFind.rs
@@ -0,0 +1,97 @@
+//
+/*
+サイズはVecにしちゃっている
+*/
+
+#[derive(Debug)]
+struct UnionFind {
+    // size= 親ならサイズ,その他は未定義. table=親を指す
+    size: Vec<usize>,
+    table: Vec<usize>,
+}
+impl UnionFind {
+    fn new(n: usize) -> Self {
+        let size = vec![1; n];
+        let mut table = vec![0; n];
+        for i in 0..n {
+            table[i] = i;
+        }
+
+        UnionFind { table: table, size }
+    }
+}
+impl UnionFind {
+    fn root(&mut self, x: usize) -> usize {
+        // 負ならそれが親
+        // 他のを指しているならたどる
+        if self.table[x] == x {
+            x
+        } else {
+            self.table[x] = self.root(self.table[x]);
+            self.table[x]
+        }
+    }
+    fn same(&mut self, a: usize, b: usize) -> bool {
+        self.root(a) == self.root(b)
+    }
+
+    fn union(&mut self, a: usize, b: usize) -> () {
+        let a_root = self.root(a);
+        let b_root = self.root(b);
+        // ここは工夫していない思考停止でbにマージ
+        self.table[a_root] = b_root;
+        self.size[b_root] += self.size[a_root];
+    }
+    // 親のサイズを返す
+    fn size(&mut self, x: usize) -> usize {
+        let ri = self.root(x);
+        self.size[ri]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::UnionFind;
+
+    #[test]
+    fn union_root_same() {
+        let mut uf = UnionFind::new(10);
+        // はじめは自分をさしてる
+        for i in 0..10 {
+            assert_eq!(uf.root(i), i);
+        }
+        // つなぐ
+        uf.union(0, 1);
+        uf.union(1, 2);
+        uf.union(2, 9);
+
+        assert_eq!(uf.same(0, 1), true);
+        assert_eq!(uf.same(0, 2), true);
+        assert_eq!(uf.same(0, 3), false);
+        assert_eq!(uf.same(0, 4), false);
+        assert_eq!(uf.same(0, 9), true);
+
+        assert_eq!(uf.size(0), 4);
+        assert_eq!(uf.size(3), 1);
+    }
+
+    #[test]
+    fn check_size() {
+        let mut uf = UnionFind::new(10);
+
+        uf.union(1, 2);
+        uf.union(4, 6);
+        uf.union(9, 1);
+        assert_eq!(uf.size(0), 1);
+        assert_eq!(uf.size(1), 3);
+        assert_eq!(uf.size(2), 3);
+        assert_eq!(uf.size(3), 1);
+        assert_eq!(uf.size(4), 2);
+        assert_eq!(uf.size(5), 1);
+        assert_eq!(uf.size(6), 2);
+        assert_eq!(uf.size(9), 3);
+    }
+
+}
+
+fn main() {}
