f314b9863e39a8c4d9c9c18826a77cc7f2968abe
KoheiAsano <kk.asano.luxy@gmail.com>
2019-12-14 10:50:00 +0900

    update template

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,62 @@
+// =========
+use std::cmp::max;
 const MOD: usize = 1000000007;
-fn palindrome(s: String, n: usize, alp: &[u8]) {
-    println!("{}", s);
-    if s.len() == 0 {
-        for letter in alp {
-            palindrome(char::from(*letter).to_string(), n - 1, alp);
-        }
-    }
-    if n > 1 {
-        for letter in alp {
-            palindrome(
-                format!("{}{}{}", *letter as char, s, *letter as char).to_string(),
-                n - 2,
-                alp,
-            );
-        }
-    }
+
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
 }
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+    // var... 変数の識別子, $t...型を一つよむ
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        //ここで繰り返し
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+    //
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+    // 配列の最後のNestではここで型が指定されてparseされる
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+// =========
 
 fn main() {
-    // matrix input
-    // let nm: Vec<usize> = read_vec();
-    // let mut map: Vec<Vec<usize>> = vec![vec![]; nm[0]];
-    // for i in 0..nm[0] {
-    //     map[i] = read_vec();
-    // }
-    // println!("{:?}", map);
-    // palindrome("".to_string(), 8, "abcd".as_bytes());
-}
-
-#[allow(dead_code)]
-fn read<T: std::str::FromStr>() -> T {
-    let mut s = String::new();
-    std::io::stdin().read_line(&mut s).ok();
-    s.trim().parse().ok().unwrap()
-}
-
-#[allow(dead_code)]
-fn read_vec<T: std::str::FromStr>() -> Vec<T> {
-    let mut s = String::new();
-    std::io::stdin().read_line(&mut s).ok();
-    s.trim()
-        .split_whitespace()
-        .map(|e| e.parse().ok().unwrap())
-        .collect()
+    input! {
+        n: usize,
+        w: usize,
+        wv: [(usize, usize);n]
+    }
 }
