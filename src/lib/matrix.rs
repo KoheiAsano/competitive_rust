c0182620086fc016e9926e4d851812944b9d8e02
KoheiAsano <kk.asano.luxy@gmail.com>
2020-01-12 15:25:30 +0900

    fix matrix

--- src/lib/matrix.rs
+++ src/lib/matrix.rs
@@ -1,23 +1,25 @@
 /*
-TODO: change vector to array
+value are supposed to be able to convert from u32
 */
 
-#[derive(Clone, Eq, PartialEq)]
+const N: usize = 3;
+
+#[derive(Clone, Copy, Eq, PartialEq)]
 struct Matrix<T>
 where
     T: Copy + Clone,
 {
-    val: Vec<Vec<T>>,
-    size: (usize, usize),
+    val: [[T; N]; N],
 }
 
+// Debug Display
 impl<T> std::fmt::Debug for Matrix<T>
 where
     T: std::fmt::Debug + Copy + Clone,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         let mut res = String::new();
-        for i in 0..self.size.0 {
+        for i in 0..N {
             res = format!("{}{:?}\n", res, self.val[i]);
         }
         write!(f, "{}", res)
@@ -25,166 +27,145 @@ where
 }
 
 // constructors
+#[allow(dead_code)]
 impl<T> Matrix<T>
 where
-    T: From<u32> + Copy + Clone,
+    T: From<i32> + Copy + Clone,
 {
-    fn i(n: usize) -> Self {
-        let mut val: Vec<Vec<T>> = vec![];
-        for i in 0..n {
-            let mut row = vec![T::from(0u32); n];
-            row[i] = T::from(1u32);
-            val.push(row);
+    fn i() -> Self {
+        let mut val: [[T; N]; N] = [[T::from(0i32); N]; N];
+        for i in 0..N {
+            val[i][i] = T::from(1i32);
         }
-        Matrix {
-            val: val,
-            size: (n, n),
-        }
+        Matrix { val: val }
     }
-    fn zero(n: usize) -> Self {
-        let val: Vec<Vec<T>> = vec![vec![T::from(0u32); n]; n];
-        Matrix {
-            val: val,
-            size: (n, n),
-        }
+    fn zero() -> Self {
+        let val: [[T; N]; N] = [[T::from(0i32); N]; N];
+        Matrix { val: val }
     }
     // generate permutation matrix
-    fn permutation(n: usize, pi: usize, pj: usize) -> Self {
-        let mut val: Vec<Vec<T>> = vec![vec![T::from(0u32); n]; n];
-        for i in 0..n {
+    fn permutation(pi: usize, pj: usize) -> Self {
+        let mut val: [[T; N]; N] = [[T::from(0i32); N]; N];
+        for i in 0..N {
             if i == pi {
-                val[i][pj] = T::from(1u32);
+                val[i][pj] = T::from(1i32);
             } else if i == pj {
-                val[i][pi] = T::from(1u32);
+                val[i][pi] = T::from(1i32);
             } else {
-                val[i][i] = T::from(1u32);
+                val[i][i] = T::from(1i32);
             }
         }
-        Matrix {
-            val: val,
-            size: (n, n),
-        }
+        Matrix { val: val }
     }
 }
 
+#[test]
+fn check_gen() {
+    println!("{:?}", Matrix::<i64>::i());
+    let m = [[1, 2, 34], [1, 2, 34], [1, 2, 34]];
+    println!("{:?}", Matrix::from(m));
+}
+
 // methods
+#[allow(dead_code)]
 impl<T> Matrix<T>
 where
-    T: std::default::Default + Copy + Clone,
+    T: From<i32> + Copy + Clone,
 {
     fn transpose(&self) -> Self {
-        let mut res = vec![vec![T::default(); self.size.0]; self.size.1];
-        for i in 0..self.size.1 {
-            for j in 0..self.size.0 {
-                res[j][i] = self.val[i][j];
+        let mut tval: [[T; N]; N] = [[T::from(0i32); N]; N];
+        for i in 0..N {
+            for j in i..N {
+                tval[j][i] = self.val[i][j];
+                tval[i][j] = self.val[j][i];
             }
         }
-        Matrix {
-            val: res,
-            size: (self.size.1, self.size.0),
-        }
+        Matrix { val: tval }
     }
 }
 
 #[test]
 fn check_permutation() {
-    let p34: Matrix<i64> = Matrix::permutation(5, 3, 4);
-    println!("{:?}", p34);
-    let p14: Matrix<i64> = Matrix::permutation(5, 1, 4);
-    println!("{:?}", p14);
-    println!("{:?}", p14.transpose());
-    let p134 = p34 * p14;
-    println!("{:?}", p134);
-    println!("{:?}", p134.transpose());
-    let p134_inv = p134.transpose();
-    println!("{:?}", p134 * p134_inv);
+    let m: Matrix<i64> = Matrix::from([[3, 2, 1], [1, 4, 5], [1, 2, 3]]);
+    let p12: Matrix<i64> = Matrix::permutation(1, 2);
+    println!("m=\n{:?}", m);
+    println!("p=\n{:?}", p12);
+    // 右から掛けると列交換
+    println!("m*p=\n{:?}", m * p12);
+    let m: Matrix<i64> = Matrix::from([[3, 2, 1], [1, 4, 5], [1, 2, 3]]);
+    let p12: Matrix<i64> = Matrix::permutation(1, 2);
+    // 左から掛けると行交換
+    println!("p*m=\n{:?}", p12 * m);
 }
 
-impl<T: Copy + Clone> From<Vec<Vec<T>>> for Matrix<T> {
-    fn from(m: Vec<Vec<T>>) -> Self {
-        let s = (m.len(), m[0].len());
-        // temporaliry trust its size
-        Matrix { val: m, size: s }
+impl<T: Copy + Clone> From<[[T; N]; N]> for Matrix<T> {
+    fn from(m: [[T; N]; N]) -> Self {
+        Matrix { val: m }
     }
 }
 
 // operations
 impl<T> std::ops::Neg for Matrix<T>
 where
-    T: std::ops::Neg<Output = T> + std::default::Default + Copy + Clone,
+    T: std::ops::Neg<Output = T> + From<i32> + Copy + Clone,
 {
     type Output = Matrix<T>;
 
     fn neg(self) -> Self::Output {
-        let mut res = vec![vec![T::default(); self.size.1]; self.size.0];
-        for i in 0..self.size.0 {
-            for j in 0..self.size.1 {
+        let mut res = [[T::from(0i32); N]; N];
+        for i in 0..N {
+            for j in 0..N {
                 res[i][j] = -self.val[i][j];
             }
         }
-        Matrix {
-            val: res,
-            size: self.size,
-        }
+        Matrix { val: res }
     }
 }
 
 // component wise
 impl<T> std::ops::Add<Matrix<T>> for Matrix<T>
 where
-    T: std::ops::Add<Output = T> + std::fmt::Debug + std::default::Default + Copy + Clone,
+    T: std::ops::Add<Output = T> + std::fmt::Debug + From<i32> + Copy + Clone,
 {
     type Output = Matrix<T>;
 
     fn add(self, rhs: Matrix<T>) -> Self::Output {
-        if self.size != rhs.size {
-            panic!("different size {:?} {:?}", self, rhs);
-        } else {
-            let mut res = vec![vec![T::default(); rhs.size.1]; self.size.0];
-            for i in 0..self.size.0 {
-                for j in 0..self.size.1 {
-                    res[i][j] = self.val[i][j] + rhs.val[i][j];
-                }
-            }
-            Matrix {
-                val: res,
-                size: self.size,
+        let mut res = [[T::from(0i32); N]; N];
+        for i in 0..N {
+            for j in 0..N {
+                res[i][j] = self.val[i][j] + rhs.val[i][j];
             }
         }
+        Matrix { val: res }
     }
 }
 
 #[test]
 fn check_ops() {
-    println!("{:?}", Matrix::<i64>::i(5) + Matrix::<i64>::i(5));
-    println!("{:?}", Matrix::<i64>::i(5) - Matrix::<i64>::i(5));
-    println!("{:?}", Matrix::<i64>::i(5) * Matrix::<i64>::i(5));
-    let m1 = Matrix::from(vec![vec![3, 2, 1], vec![1, 4, 5], vec![1, 2, 3]]);
-    let m2 = Matrix::from(vec![vec![1, 2, 3], vec![3, 2, 1], vec![1, 3, 2]]);
+    println!("{:?}", Matrix::<i64>::i() + Matrix::<i64>::i());
+    println!("{:?}", Matrix::<i64>::i() - Matrix::<i64>::i());
+    println!("{:?}", Matrix::<i64>::i() * Matrix::<i64>::i());
+    let m1 = Matrix::from([[3, 2, 1], [1, 4, 5], [1, 2, 3]]);
+    let m2 = Matrix::from([[1, 2, 3], [3, 2, 1], [1, 3, 2]]);
+    println!("{:?}", m1 * m2);
+    println!("{:?}", m1 * m2);
 }
 
 // component wise
 impl<T> std::ops::Sub<Matrix<T>> for Matrix<T>
 where
-    T: std::ops::Sub<Output = T> + std::fmt::Debug + std::default::Default + Copy + Clone,
+    T: std::ops::Sub<Output = T> + std::fmt::Debug + From<i32> + Copy + Clone,
 {
     type Output = Matrix<T>;
 
     fn sub(self, rhs: Matrix<T>) -> Self::Output {
-        if self.size != rhs.size {
-            panic!("different size {:?} {:?}", self, rhs);
-        } else {
-            let mut res = vec![vec![T::default(); rhs.size.1]; self.size.0];
-            for i in 0..self.size.0 {
-                for j in 0..self.size.1 {
-                    res[i][j] = self.val[i][j] - rhs.val[i][j];
-                }
-            }
-            Matrix {
-                val: res,
-                size: self.size,
+        let mut res = [[T::from(0i32); N]; N];
+        for i in 0..N {
+            for j in 0..N {
+                res[i][j] = self.val[i][j] - rhs.val[i][j];
             }
         }
+        Matrix { val: res }
     }
 }
 
@@ -195,37 +176,34 @@ where
     T: std::ops::Add<Output = T>
         + std::ops::Mul<Output = T>
         + std::fmt::Debug
-        + std::default::Default
+        + From<i32>
         + Copy
         + Clone,
 {
     type Output = Matrix<T>;
 
     fn mul(self, rhs: Matrix<T>) -> Self::Output {
-        if self.size != rhs.size {
-            panic!("different size {:?} {:?}", self, rhs);
-        } else {
-            let mut res = vec![vec![T::default(); rhs.size.1]; self.size.0];
-            for i in 0..self.size.0 {
-                for j in 0..self.size.1 {
-                    for k in 0..rhs.size.1 {
-                        res[i][k] = res[i][k] + (self.val[i][j] * rhs.val[j][k]);
-                    }
+        let mut res = [[T::from(0i32); N]; N];
+        for i in 0..N {
+            for j in 0..N {
+                for k in 0..N {
+                    res[i][k] = res[i][k] + (self.val[i][j] * rhs.val[j][k]);
                 }
             }
-            Matrix {
-                val: res,
-                size: self.size,
-            }
         }
+        Matrix { val: res }
     }
 }
 
-#[test]
-fn check_gen() {
-    println!("{:?}", Matrix::<i64>::i(5));
-    let m = vec![vec![1, 2, 34], vec![1, 2, 34], vec![1, 2, 34]];
-    println!("{:?}", Matrix::from(m));
-}
-
 fn main() {}
+
+#[test]
+fn check_complex() {
+    let i = Complex::i();
+    let cm: Matrix<Complex> = Matrix::from([
+        [i, Complex::zero(), Complex::zero()],
+        [Complex::zero(), i, Complex::zero()],
+        [Complex::zero(), Complex::zero(), i],
+    ]);
+    println!("{:?}", cm * cm);
+}
