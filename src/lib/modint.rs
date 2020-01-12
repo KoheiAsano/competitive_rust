26a31bb3c232f8f95411b92c4abc42fe6883ccb0
KoheiAsano <kk.asano.luxy@gmail.com>
2020-01-12 16:08:37 +0900

    rewrite modint

--- src/lib/modint.rs
+++ src/lib/modint.rs
@@ -1,120 +1,136 @@
-const MOD: u64 = 998_244_353;
+macro_rules! define_modint {
+    ($n:ident,$m:expr) => {
+        #[derive(Clone, Copy)]
+        struct $n(u64);
 
-#[derive(Clone, Copy)]
-struct ModInt(u64);
-
-impl ModInt {
-    pub fn new(x: u64) -> ModInt {
-        ModInt(x % MOD)
-    }
-
-    pub fn pow(mut self, mut n: u64) -> ModInt {
-        let mut ret = ModInt::new(1);
-        while n > 0 {
-            if n & 1 == 1 {
-                ret *= self;
+        #[allow(dead_code)]
+        impl $n {
+            pub fn new(x: u64) -> $n {
+                $n(x % $m)
             }
-            self *= self;
-            n >>= 1;
+
+            pub fn pow(mut self, mut n: u64) -> $n {
+                let mut ret = $n::new(1);
+                while n > 0 {
+                    if n & 1 == 1 {
+                        ret *= self;
+                    }
+                    self *= self;
+                    n >>= 1;
+                }
+                ret
+            }
+
+            pub fn inv(&self) -> $n {
+                self.pow($m - 2)
+            }
+            // when m is not prime
+            // pub fn inv2(&self) -> $n {
+            //     let mut ab = (self, $n::new(0));
+            //     let mut uv = ($n::new(1), $n::new(0));
+            //     let mut t: i64;
+            //     while ab.1 != 0 {
+            //         t = ab.0 / ab.1;
+            //         ab = (ab.1, ab.0 - t * ab.1);
+            //         uv = (uv.1, uv.0 - t * uv.1);
+            //     }
+            //     if ab.0 != 1 {
+            //         panic!("{} and {} are not coprime g={}", a, m, ab.0);
+            //     }
+            //     let inv = uv.0 % m as i64;
+            //     if inv < 0 {
+            //         inv + m as i64
+            //     } else {
+            //         inv as i64
+            //     }
+            // }
         }
-        ret
-    }
 
-    pub fn inv(&self) -> ModInt {
-        self.pow(MOD - 2)
-    }
-}
+        impl std::convert::From<u64> for $n {
+            fn from(x: u64) -> $n {
+                $n::new(x)
+            }
+        }
 
+        // Binary operator
+        impl std::ops::Add for $n {
+            type Output = $n;
+            fn add(self, rhs: $n) -> Self::Output {
+                $n::new(self.0 + rhs.0)
+            }
+        }
 
-impl std::convert::From<u64> for ModInt {
-    fn from(x: u64) -> ModInt {
-        ModInt::new(x)
-    }
-}
+        impl std::ops::Sub for $n {
+            type Output = $n;
+            fn sub(self, rhs: $n) -> Self::Output {
+                if self.0 >= rhs.0 {
+                    $n::new(self.0 - rhs.0)
+                } else {
+                    $n::new($m - rhs.0 + self.0)
+                }
+            }
+        }
 
-// Assign
-impl std::ops::AddAssign for ModInt {
-    fn add_assign(&mut self, rhs: ModInt) {
-        self.0 += rhs.0;
-        if self.0 >= MOD {
-            self.0 -= MOD;
-        };
-    }
-}
+        impl std::ops::Mul for $n {
+            type Output = $n;
+            fn mul(self, rhs: $n) -> Self::Output {
+                $n::new(self.0 * rhs.0)
+            }
+        }
 
-impl std::ops::SubAssign for ModInt {
-    fn sub_assign(&mut self, rhs: ModInt) {
-        self.0 += MOD - rhs.0;
-        if self.0 >= MOD {
-            self.0 -= MOD;
-        };
-    }
-}
+        impl std::ops::Div for $n {
+            type Output = $n;
+            fn div(self, rhs: $n) -> Self::Output {
+                $n::new(self.0 / rhs.0)
+            }
+        }
 
-impl std::ops::MulAssign for ModInt {
-    fn mul_assign(&mut self, rhs: ModInt) {
-        self.0 = (self.0 as u64) * (rhs.0 as u64) % (MOD as u64);
-    }
-}
+        // Assign
+        impl std::ops::AddAssign for $n {
+            fn add_assign(&mut self, rhs: $n) {
+                *self = *self + rhs;
+            }
+        }
 
-impl std::ops::DivAssign for ModInt {
-    fn div_assign(&mut self, rhs: ModInt) {
-        *self *= rhs.inv();
-    }
-}
+        impl std::ops::SubAssign for $n {
+            fn sub_assign(&mut self, rhs: $n) {
+                *self = *self - rhs;
+            }
+        }
 
-// Binary operator
-impl std::ops::Add for ModInt {
-    type Output = ModInt;
-    fn add(mut self, rhs: ModInt) -> Self::Output {
-        self += rhs;
-        self
-    }
-}
+        impl std::ops::MulAssign for $n {
+            fn mul_assign(&mut self, rhs: $n) {
+                *self = *self * rhs;
+            }
+        }
 
-impl std::ops::Sub for ModInt {
-    type Output = ModInt;
-    fn sub(mut self, rhs: ModInt) -> Self::Output {
-        self -= rhs;
-        self
-    }
-}
+        impl std::ops::DivAssign for $n {
+            fn div_assign(&mut self, rhs: $n) {
+                *self = *self / rhs;
+            }
+        }
 
-impl std::ops::Mul for ModInt {
-    type Output = ModInt;
-    fn mul(mut self, rhs: ModInt) -> Self::Output {
-        self *= rhs;
-        self
-    }
+        impl std::fmt::Display for $n {
+            fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+                write!(f, "{}", self.0)
+            }
+        }
+        impl std::fmt::Debug for $n {
+            fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+                write!(f, "{}", self.0)
+            }
+        }
+    };
 }
-
-impl std::ops::Div for ModInt {
-    type Output = ModInt;
-    fn div(mut self, rhs: ModInt) -> Self::Output {
-        self /= rhs;
-        self
-    }
-}
-
-impl std::fmt::Display for ModInt {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-impl std::fmt::Debug for ModInt {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
+define_modint!(ModInt998244353, 998244353);
 
 mod tests {
     use super::*;
     #[test]
     fn check_modint() {
-        let a = ModInt::new(1);
-        let b = ModInt::new(1);
-        println!("{:?}", a + b);
-        println!("{:?}", a);
+        let mut a = ModInt998244353::new(1);
+        let b = ModInt998244353::new(1);
     }
-
 }
+
+fn main() {}
