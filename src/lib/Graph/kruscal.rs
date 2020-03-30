119edf368b268c3404264664f345bec30da6d7cf
KoheiAsano <kk.asano.luxy@gmail.com>
2020-03-30 12:31:13 +0900

    Struct to module

--- src/lib/Graph/kruscal.rs
+++ src/lib/Graph/kruscal.rs
@@ -1,92 +1,85 @@
 // use std::cmp::Ordering;
 // ======UnionFind======
-#[derive(Debug)]
-struct UnionFind {
-    // size= 親なら負のサイズ、子なら親
-    // number= 集合の数
-    table: Vec<i64>,
-    number: usize,
-}
-impl UnionFind {
-    fn new(n: usize) -> Self {
-        let mut table = vec![0; n];
-        for i in 0..n {
-            table[i] = -1;
-        }
-        UnionFind {
-            table: table,
-            number: n,
-        }
-    }
-}
-impl UnionFind {
-    fn root(&mut self, x: usize) -> usize {
-        // 負ならそれが親
-        // 他のを指しているならたどる
-        if self.table[x] < 0 {
-            x
-        } else {
-            let tmp = self.root(self.table[x] as usize);
-            self.table[x] = tmp as i64;
-            tmp
-        }
-    }
-    fn same(&mut self, a: usize, b: usize) -> bool {
-        self.root(a) == self.root(b)
-    }
-
-    fn union(&mut self, a: usize, b: usize) -> () {
-        let a_root = self.root(a);
-        let b_root = self.root(b);
-
-        if a_root == b_root {
-            return ();
-        }
-        // 負なので小さい法が大きい. 大きい方につける
-        if self.table[a_root] > self.table[b_root] {
-            self.table[b_root] += self.table[a_root];
-            self.table[a_root] = b_root as i64;
-        } else {
-            self.table[a_root] += self.table[b_root];
-            self.table[b_root] = a_root as i64;
-        }
-        self.number -= 1;
-    }
-    // 親のサイズを返す
-    fn size(&mut self, x: usize) -> usize {
-        let ri = self.root(x);
-        -self.table[ri] as usize
-    }
-    fn count(&self) -> usize {
-        self.number
-    }
-}
 
 // ======Kruskal======
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-struct Edge {
-    from: usize,
-    to: usize,
-    cost: i64,
-}
 
-impl Ord for Edge {
-    fn cmp(&self, other: &Edge) -> std::cmp::Ordering {
-        self.cost.cmp(&other.cost)
+mod Kruscal {
+    #[derive(Debug)]
+    struct UnionFind {
+        // size= 親なら負のサイズ、子なら親
+        // number= 集合の数
+        table: Vec<i64>,
+        number: usize,
     }
-}
-
-impl PartialOrd for Edge {
-    fn partial_cmp(&self, other: &Edge) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(&other))
+    impl UnionFind {
+        fn new(n: usize) -> Self {
+            let table = vec![-1; n];
+            UnionFind {
+                table: table,
+                number: n,
+            }
+        }
     }
-}
+    impl UnionFind {
+        fn root(&mut self, x: usize) -> usize {
+            let par = self.table[x];
+            if par < 0 {
+                x
+            } else {
+                let tmp = self.root(par as usize);
+                self.table[x] = tmp as i64;
+                tmp
+            }
+        }
+        fn same(&mut self, a: usize, b: usize) -> bool {
+            self.root(a) == self.root(b)
+        }
 
-#[derive(Debug)]
-struct Kruscal {}
-impl Kruscal {
-    // build minimum spanning tree
-    fn build(v: usize, edges: &mut Vec<Edge>) -> (Vec<Edge>, i64) {
+        fn union(&mut self, a: usize, b: usize) -> () {
+            let a_root = self.root(a);
+            let b_root = self.root(b);
+
+            if a_root == b_root {
+                return ();
+            }
+            // 負なので小さい法が大きい. 大きい方につける
+            if self.table[a_root] > self.table[b_root] {
+                self.table[b_root] += self.table[a_root];
+                self.table[a_root] = b_root as i64;
+            } else {
+                self.table[a_root] += self.table[b_root];
+                self.table[b_root] = a_root as i64;
+            }
+            self.number -= 1;
+        }
+        // 親のサイズを返す
+        fn size(&mut self, x: usize) -> usize {
+            let ri = self.root(x);
+            -self.table[ri] as usize
+        }
+        fn count(&self) -> usize {
+            self.number
+        }
+    }
+    #[derive(Debug, Copy, Clone, Eq, PartialEq)]
+    pub struct Edge {
+        pub from: usize,
+        pub to: usize,
+        pub cost: i64,
+    }
+
+    impl Ord for Edge {
+        fn cmp(&self, other: &Edge) -> std::cmp::Ordering {
+            self.cost.cmp(&other.cost)
+        }
+    }
+
+    impl PartialOrd for Edge {
+        fn partial_cmp(&self, other: &Edge) -> Option<std::cmp::Ordering> {
+            Some(self.cmp(&other))
+        }
+    }
+    pub fn build(v: usize, edges: &mut Vec<Edge>) -> (Vec<Edge>, i64) {
         let mut uf = UnionFind::new(v);
         // sort ascending order
         edges.sort();
@@ -105,7 +98,6 @@ impl Kruscal {
         (res_tree, res)
     }
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
